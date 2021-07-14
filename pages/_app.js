aaf771ea5ead25d4adaaa0621b0fae179a1aa03a
franciscoguinarte <francisco.guinarte@outlook.com.br>
2021-07-14 01:02:03 -0300

    add global style and css reset

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
+  * {
+    margin : 0;
+    padding : 0;
+    box-sizing : border-box;
+  }
+
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family : sans-serif;
+    background-color : #D9E6F6;
   }
 `
 
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,33 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+// const Title = styled.h1`
+//   font-size: 50px;
+//   color: ${({ theme }) => theme.colors.primary};
+// `
+
+const Box = styled.div`
+  background: #FFFFFF;
+  border-radius: 8px;
 `
 
+
+
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+  
+  <main>
+    <Box>
+      Imagem
+    </Box>
+    <Box>
+      Bem vindos
+    </Box>
+    <Box>
+      Comunidades
+    </Box>
+    
+          
+  </main>
+  
+  )
 }

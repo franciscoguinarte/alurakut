0f40bb292948fe5899108965ab7adcc01eb012c4
franciscoguinarte <francisco.guinarte@outlook.com.br>
2021-07-17 17:00:43 -0300

    feat: login page and authentication

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import MainGrid from "../src/components/MainGrid";
 import Box from "../src/components/Box";
+import nookies from 'nookies'
 import { AlurakutMenu, OrkutNostalgicIconSet, AlurakutProfileSidebarMenuDefault } from "../src/lib/AlurakutCommons";
 import { ProfileRelationsBoxWrapper } from "../src/components/ProfileRelations";
 import React from "react";
+import jwt from 'jsonwebtoken'
 
 function ProfileSidebar(propriedades) {
   return (
@@ -44,11 +46,11 @@ function ProfileRelationsBox(propriedades) {
   )
 }
 
-export default function Home() {
+export default function Home(props) {
 
 
 
-  const githubUser = "franciscoguinarte";
+  const githubUser = props.githubUser;
   const [comunidades, setComunidades] = React.useState([ ])
   const pessoasFavoritas = [
     "juunegreiros",
@@ -212,9 +214,35 @@ export default function Home() {
             </ul>
           </ProfileRelationsBoxWrapper>
 
-          <Box>Comunidades</Box>
+          
         </div>
       </MainGrid>
     </>
   );
+}
+export async function getServerSideProps(context) {
+  const cookies = nookies.get(context)
+  const token = cookies.USER_TOKEN;
+  const { isAuthenticated } = await fetch("http://localhost:3000/api/auth", {
+    headers: {
+      Authorization: token,
+    },
+  })
+  .then((resposta) => resposta.json())
+
+  if(!isAuthenticated) { 
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      }
+    }
+  }
+
+  const { githubUser } = jwt.decode(token);
+  return {
+    props: {
+      githubUser
+    }, // will be passed to the page component as props
+  }
 }
